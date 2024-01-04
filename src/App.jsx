27719cff01c1331dadb00b634a65e05089e9b70e
AlessandroTedde <148778362+AlessandroTedde@users.noreply.github.com>
2024-01-04 11:26:39 +0100

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TopBar from "./Components/TopBar";
 import ActionBar from "./Components/ActionBar";
 // import ImgRow from "./Components/ImgRow";
 import Carousel from "./Components/Carousel";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Carousel title="Trending Now" queryParam="superman" />
       <Carousel title="Watch It Again" queryParam="batman" />
       <Carousel title="New Releases" queryParam="aquaman" />
+      <Footer />
     </div>
   );
 }
